2dc5aa53acec815379ec9cb4b2627151c6190db3
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2026-02-06 12:51:37 +0100

    add call of post migration signal on start up to insert default values to django tables

--- docker/backend/start.sh
+++ docker/backend/start.sh
@@ -6,6 +6,9 @@ export PYTHONPATH=/opt/pandaui/backend
 # Activate virtualenv
 source /opt/pandaui/venv/bin/activate
 
+echo "Marking migrations as applied (fake) to trigger post migration signals"
+python manage.py migrate --fake --noinput
+
 # Start NGINX
 echo "Starting NGINX..."
 nginx -g "daemon off;" &
