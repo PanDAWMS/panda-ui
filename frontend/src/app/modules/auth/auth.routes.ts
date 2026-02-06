d9f0794f738bb507e8b5e0fa2621a61d35677292
Tatiana Korchuganova <tatiana.korchuganova@cern.ch>
2026-02-06 18:25:13 +0100

    fix non closing user menu when logout

--- frontend/src/app/modules/auth/auth.routes.ts
+++ frontend/src/app/modules/auth/auth.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoginCallbackComponent } from './components/login-callback/login-callback.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { LogoutComponent } from './components/logout/logout.component';
 
 export const authRoutes: Routes = [
   { path: 'login', component: LoginComponent, title: 'Login' },
   { path: 'login/callback', component: LoginCallbackComponent, title: 'Login' },
-  { path: 'logout', component: LogoutComponent, title: 'Logout' },
   { path: 'user/profile', component: UserProfileComponent, title: 'User Profile' },
 ];
